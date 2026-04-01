5c5ebd2132e62fe34b88d29c0667e07081640651
ome_chawanat <98893062+omechawanat@users.noreply.github.com>
2026-04-01 09:52:02 +0000

    sadddddd

--- main.cpp
+++ main.cpp
@@ -3,15 +3,18 @@ using namespace std;
 #include "student.h"
 
 int main(){
-MU_Person m;
- 
-	//student m1(6613121,2,"Vivi"),m2(112),m3;
- // student m1(6613118,3.9,"Atom");
-//student m2(6613112),m3,*p;
-  //m1.display(); // id gpa name
-  cout<<endl<<"======="<<endl;
-  //m1.display_person(); //id
+  MU_person m(999);
+  m.display_person(); //id
 
+  student m1(6613121,2,"Vivi",666),m2(114),m3;
+  //m1.display(); // id gpa name
+  m1.display();
+  m2.display();
+  m3.display();
+// student m1(6613118,3.9,"Atom");
+//student m2(6613112),m3,*p;
+
+  cout<<endl<<"======="<<endl;
 //  p=new student(6613265,1.7,"Hok");
  // delete p;
   
--- makefile
+++ makefile
@@ -1,2 +1,8 @@
-target: MU_Person.h student.h main.cpp
-	g++ main.cpp -o inh
+compile:LL_test.cpp
+	g++ LL_test.cpp LL.cpp NODE.cpp -o inh
+run: inh
+	./inh 012  3.0 pupha 120  3.1 ryan
+
+
+clean: inh
+	rm inh
